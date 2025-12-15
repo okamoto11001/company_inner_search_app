d8370a1aa5f32813a7fb32c1b7c2ed27b786a0d8
okamoto <okamoto@kri.co.jp>
2025-12-15 16:40:20 +0900

    first commit.

--- utils.py
+++ utils.py
@@ -9,9 +9,10 @@ import os
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema import HumanMessage
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 import constants as ct
 
